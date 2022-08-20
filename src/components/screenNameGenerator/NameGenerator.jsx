eece26d4ee69e2ef519c62b085a36f512f37f3dc
NBryant83 <73191760+NBryant83@users.noreply.github.com>
2022-08-20 19:23:51 -0400

    added screenNameGenerator folder and files

---
+++ src/components/screenNameGenerator/NameGenerator.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./nameGenerator.css"
+const NameGenerator = () => {
+
+
+   return (
+      <>
+         <input
+            type="button"
+            id="nameGenerator"
+         >Generate Screen Name
+         
+         </input>
+      </>
+   )
+}
+
+export default NameGenerator
